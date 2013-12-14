7fbf02c14411d2df00df66e3ebbb1a085a25d52b
Arcanine <sfyire@gmail.com>
2013-12-14 16:39:06 +0000

    test

--- tests/runTests.php
+++ tests/runTests.php
@@ -1,6 +1,6 @@
 <?php
-echo '\n';
-echo '<<< Starting Unit Tests'.'\n';
+echo "\n";
+echo '<<< Starting Unit Tests'."\n";
 $f3 = require_once 'E:/xampp/htdocs/sitebuilder/vendor/bcosca/fatfree/lib/base.php';
 (new init)->root('E:/xampp/htdocs/sitebuilder/app')->db()->run();
 $test = new Test;
@@ -12,7 +12,7 @@ foreach($test->results() as $k=>$v){
 	if (!$v['status']){
 		echo $v['text'].' -- '.chr(27).'[41mFailed'.chr(27).'[0m on '.$v['source'];
 		die(1);
-	}else echo $v['text'].' -- '.chr(27).'[42mPassed'.chr(27).'[0m on '.basename($v['source']).'\n';
+	}else echo $v['text'].' -- '.chr(27).'[42mPassed'.chr(27).'[0m on '.basename($v['source'])."\n";
 }
 
 echo '<<< All Tests '.chr(27).'[42mSuccessful'.chr(27).'[0m';
