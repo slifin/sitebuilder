041201680d38264646608dc76fee9342bd462104
Arcanine <sfyire@gmail.com>
2013-12-14 16:28:32 +0000

    test

--- tests/runTests.php
+++ tests/runTests.php
@@ -4,8 +4,8 @@ echo '<<< Starting Unit Tests'.PHP_EOL;
 $f3 = require_once 'E:/xampp/htdocs/sitebuilder/vendor/bcosca/fatfree/lib/base.php';
 (new init)->root('E:/xampp/htdocs/sitebuilder/app')->db()->run();
 $test = new Test;
-foreach(glob('./*.php') as $filename){
-	if ($filename == './'.basename(__FILE__)) continue;
+foreach(glob('E:/xampp/htdocs/sitebuilder/tests/*.php') as $filename){
+	if (basename($filename) == basename(__FILE__)) continue;
 	include $filename;
 }
 foreach($test->results() as $k=>$v){
@@ -15,4 +15,5 @@ foreach($test->results() as $k=>$v){
 	}else echo $v['text'].' -- '.chr(27).'[42mPassed'.chr(27).'[0m on '.basename($v['source']).PHP_EOL;
 }
 echo '<<< All Tests '.chr(27).'[42mSuccessful'.chr(27).'[0m';
+
 die(0);
